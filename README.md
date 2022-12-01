b4e824fc4b2728d9a57d68b4ee6f8b9d2f47ba45
Sebastian Andersson <sebastian@bittr.nu>
2022-12-01 08:40:19 +0100

    Make project REUSE compliant

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
+<!--
+SPDX-FileCopyrightText: 2022 Sebastian Andersson <sebastian@bittr.nu>
+
+SPDX-License-Identifier: GPL-3.0-or-later
+-->
+
 # aoc_2022
 Advent of Code 2022
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2022 Sebastian Andersson <sebastian@bittr.nu>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 use aoc_runner_derive::aoc_lib;
 
 mod day1;
