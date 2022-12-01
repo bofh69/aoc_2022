f0a5a72671614adbc7ac4e973a25b122f3b522bd
Sebastian Andersson <sebastian@bittr.nu>
2022-12-01 10:11:53 +0100

    Fix fmt & clippy issues

--- src/day1.rs
+++ src/day1.rs
@@ -7,10 +7,10 @@ use aoc_runner_derive::{aoc, aoc_generator};
 
 #[aoc_generator(day1)]
 pub fn input_generator(input: &str) -> Vec<i32> {
-    let mut result = vec!();
+    let mut result = vec![];
     let mut sum = 0;
     for line in input.lines() {
-        if line == "" {
+        if line.is_empty() {
             result.push(sum);
             sum = 0;
         } else {
@@ -27,7 +27,7 @@ pub fn solve_part1(data: &[i32]) -> i32 {
 
 #[aoc(day1, part2)]
 pub fn solve_part2(data: &[i32]) -> i32 {
-    let mut result : Vec<_> = data.into();
+    let mut result: Vec<_> = data.into();
 
     result.sort();
     result.iter().rev().take(3).sum::<i32>()
