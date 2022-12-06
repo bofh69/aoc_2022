ab05eefdba2f4d430dba5ef6626c877c655d64f7
Sebastian Andersson <sebastian@bittr.nu>
2022-12-06 11:04:49 +0100

    Slight performance improvement with const generics

--- src/day6.rs
+++ src/day6.rs
@@ -13,27 +13,27 @@ pub fn input_generator(input: &str) -> Vec<InputType> {
     input.as_bytes().into()
 }
 
-fn solve_for(data: &[InputType], n: usize) -> SolutionType {
-    let len = data.len() /* - n */; // Adding -n makes it 14% slower!
+fn solve_for<const N: usize>(data: &[InputType]) -> SolutionType {
+    let len = data.len() /* - N */; // Adding -n makes it 14% slower!
     'next: for i in 0..len {
-        for j in 0..n-1 {
-            for k in j + 1..n {
+        for j in 0..N-1 {
+            for k in j + 1..N {
                 if data[i + j] == data[i + k] {
                     continue 'next;
                 }
             }
         }
-        return i + n;
+        return i + N;
     }
     usize::MAX
 }
 
 #[aoc(day6, part1)]
 pub fn solve_part1(data: &[InputType]) -> SolutionType {
-    solve_for(data, 4)
+    solve_for::<4>(data)
 }
 
 #[aoc(day6, part2)]
 pub fn solve_part2(data: &[InputType]) -> SolutionType {
-    solve_for(data, 14)
+    solve_for::<14>(data)
 }
