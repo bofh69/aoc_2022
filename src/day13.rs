f60d1a81b4145f138f60e86661c61d15eaef0437
Sebastian Andersson <sebastian@bittr.nu>
2022-12-13 16:22:35 +0100

    Quick & dirty day 13

---
+++ src/day13.rs
@@ -0,0 +1,230 @@
+// SPDX-FileCopyrightText: 2022 Sebastian Andersson <sebastian@bittr.nu>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+use aoc_runner_derive::{aoc, aoc_generator};
+
+#[derive(Debug, Clone)]
+pub enum List {
+    List(Vec<List>),
+    Number(u8),
+}
+
+impl Eq for List {}
+
+impl PartialEq for List {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (List::Number(a), List::Number(b)) => a == b,
+            (List::List(a), List::List(b)) => a == b,
+            _ => false,
+        }
+    }
+}
+
+impl Ord for List {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        match right_order(self, other) {
+            Some(true) => std::cmp::Ordering::Less,
+            None => std::cmp::Ordering::Equal,
+            Some(false) => std::cmp::Ordering::Greater,
+        }
+    }
+}
+
+impl PartialOrd for List {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+type InputType = (List, List);
+type SolutionType = i32;
+
+fn is_digit(c: u8) -> bool {
+    (b'0'..=b'9').contains(&c)
+}
+
+fn parse_list_members(line: &[u8], pos: &mut usize) -> List {
+    let mut result = vec![];
+    while line[*pos] != b']' {
+        if line[*pos] == b'[' {
+            result.push(parse_bytes(line, pos));
+            if line[*pos] == b',' {
+                *pos += 1;
+                continue;
+            }
+        } else if is_digit(line[*pos]) {
+            result.push(parse_number(line, pos));
+            if line[*pos] == b',' {
+                *pos += 1;
+                continue;
+            }
+        }
+    }
+    List::List(result)
+}
+
+fn parse_number(line: &[u8], pos: &mut usize) -> List {
+    let mut result = 0;
+    while is_digit(line[*pos]) {
+        result = result * 10 + (line[*pos] - b'0');
+        *pos += 1;
+    }
+    List::Number(result)
+}
+
+fn parse_bytes(line: &[u8], pos: &mut usize) -> List {
+    if line[*pos] == b'[' {
+        *pos += 1;
+        let result = parse_list_members(line, pos);
+        if line[*pos] != b']' {
+            panic!("Expected ] as position {pos}");
+        }
+        *pos += 1;
+        return result;
+    } else if is_digit(line[*pos]) {
+        parse_number(line, pos)
+    } else {
+        panic!("Unknown data {}", line[*pos]);
+    }
+}
+
+fn parse_line(line: &str) -> List {
+    let line = line.as_bytes();
+    parse_bytes(line, &mut 0)
+}
+
+#[aoc_generator(day13)]
+pub fn input_generator(input: &str) -> Vec<InputType> {
+    let mut result = vec![];
+    let mut lines = input.lines();
+    while let Some(line) = lines.next() {
+        let line2 = lines.next().unwrap();
+        let line = parse_line(line);
+        let line2 = parse_line(line2);
+        result.push((line, line2));
+        lines.next();
+    }
+    result
+}
+
+fn right_order(a: &List, b: &List) -> Option<bool> {
+    match (a, b) {
+        (List::Number(a), List::Number(b)) => {
+            if a < b {
+                Some(true)
+            } else if a == b {
+                None
+            } else {
+                Some(false)
+            }
+        }
+        (List::List(a), List::List(b)) => {
+            for i in 0..a.len() {
+                if i >= b.len() {
+                    return Some(false);
+                }
+                match right_order(&a[i], &b[i]) {
+                    Some(val) => return Some(val),
+                    None => (),
+                }
+            }
+            if a.len() < b.len() {
+                Some(true)
+            } else if a.len() == b.len() {
+                None
+            } else {
+                Some(false)
+            }
+        }
+        (List::Number(a), List::List(_)) => right_order(&List::List(vec![List::Number(*a)]), b),
+        (List::List(_), List::Number(b)) => right_order(a, &List::List(vec![List::Number(*b)])),
+    }
+}
+
+#[aoc(day13, part1)]
+pub fn solve_part1(data: &[InputType]) -> SolutionType {
+    let mut result = 0;
+    for i in 0..data.len() {
+        if right_order(&data[i].0, &data[i].1).unwrap() {
+            let i = i + 1;
+            result += i;
+        }
+    }
+    result as SolutionType
+}
+
+#[aoc(day13, part2)]
+pub fn solve_part2(data: &[InputType]) -> SolutionType {
+    let mut rows = vec![];
+    rows.push(parse_line("[[2]]"));
+    rows.push(parse_line("[[6]]"));
+    for pair in data {
+        rows.push(pair.0.clone());
+        rows.push(pair.1.clone());
+    }
+    rows.sort();
+    let mut first = 0;
+    let mut second = 0;
+    for i in 0..rows.len() {
+        if rows[i] == List::List(vec![List::List(vec![List::Number(2)])]) {
+            first = i + 1;
+        } else if rows[i] == List::List(vec![List::List(vec![List::Number(6)])]) {
+            second = i + 1;
+        }
+    }
+    (first * second) as SolutionType
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    pub fn empty() {
+        assert_eq!(List::List(vec![]), parse_line("[]"));
+    }
+
+    #[test]
+    pub fn list_with_number() {
+        assert_eq!(List::List(vec![List::Number(1)]), parse_line("[1]"));
+    }
+
+    #[test]
+    pub fn list_with_numbers() {
+        assert_eq!(
+            List::List(vec![List::Number(1), List::Number(2)]),
+            parse_line("[1,2]")
+        );
+    }
+
+    #[test]
+    pub fn list_with_empty_list() {
+        assert_eq!(List::List(vec![List::List(vec![])]), parse_line("[[]]"));
+    }
+
+    #[test]
+    pub fn list_with_list_with_number() {
+        assert_eq!(
+            List::List(vec![List::List(vec![List::Number(10)])]),
+            parse_line("[[10]]")
+        );
+    }
+
+    #[test]
+    pub fn list_with_number_and_empty_list() {
+        assert_eq!(
+            List::List(vec![List::Number(7), List::List(vec![])]),
+            parse_line("[7,[]]")
+        );
+    }
+
+    #[test]
+    pub fn list_with_empty_list_and_number() {
+        assert_eq!(
+            List::List(vec![List::List(vec![]), List::Number(7)]),
+            parse_line("[[],7]")
+        );
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,7 @@ mod day9;
 mod day10;
 mod day11;
 mod day12;
-// mod day13;
+mod day13;
 // mod day14;
 // mod day15;
 // mod day16;
