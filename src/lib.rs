ef8a406411c9d5e350ecb518cc3a7e0b0ab3d651
Sebastian Andersson <sebastian@bittr.nu>
2022-12-14 10:26:09 +0100

    Rename day 1..9 to sort better

--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,15 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 use aoc_runner_derive::aoc_lib;
 
-mod day1;
-mod day2;
-mod day3;
-mod day4;
-mod day5;
-mod day6;
-mod day7;
-mod day8;
-mod day9;
+mod day01;
+mod day02;
+mod day03;
+mod day04;
+mod day05;
+mod day06;
+mod day07;
+mod day08;
+mod day09;
 mod day10;
 mod day11;
 mod day12;
