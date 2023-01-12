e29c5b0251a358ccae2c538d28c6fa884b3ff7a1
Sebastian Andersson <sebastian@bittr.nu>
2023-01-12 10:15:25 +0100

    Input parsing for day19

---
+++ src/day19.rs
@@ -0,0 +1,25 @@
+// SPDX-FileCopyrightText: 2022 Sebastian Andersson <sebastian@bittr.nu>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+use aoc_runner_derive::{aoc, aoc_generator};
+// use rayon::prelude::*;
+
+type CountType = u16;
+type InputType = (u8, CountType, CountType, CountType, CountType, CountType, CountType);
+type SolutionType = i32;
+
+#[aoc_generator(day19)]
+pub fn input_generator(input: &str) -> Vec<InputType> {
+    input.lines().map(|line| sscanf::sscanf!(line, "Blueprint {u8}: Each ore robot costs {CountType} ore. Each clay robot costs {CountType} ore. Each obsidian robot costs {CountType} ore and {CountType} clay. Each geode robot costs {CountType} ore and {CountType} obsidian.").unwrap()).collect()
+}
+
+#[aoc(day19, part1)]
+pub fn solve_part1(data: &[InputType]) -> SolutionType {
+    data.len() as SolutionType
+}
+
+#[aoc(day19, part2)]
+pub fn solve_part2(data: &[InputType]) -> SolutionType {
+    data.len() as SolutionType
+}
--- src/lib.rs
+++ src/lib.rs
@@ -21,7 +21,7 @@ mod day15;
 mod day16;
 mod day17;
 mod day18;
-// mod day19;
+mod day19;
 // mod day20;
 // mod day21;
 // mod day22;
